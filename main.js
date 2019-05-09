5420f41b1197dd61810010c888c42a36e596c97d
matt henley <mattbhenley@gmail.com>
2019-05-09 16:54:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const { Rectangle, Color } = require("scenegraph");
+const commands = require("commands");
+
+function myPluginCommand(selection) {
+
+}
+
+module.exports = {
+    commands: {
+        myPluginCommand
+    }
+};
